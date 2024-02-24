f607ab1a29553dd2f42f8129a599661eaa04261e
IvanMarte <ivanbiasfx@gmail.com>
2024-02-23 23:05:53 -0600

    Section 2 Finish Author Phase animation

--- main.js
+++ main.js
@@ -175,3 +175,32 @@ ScrollTrigger.create({
     }
   }
 });
+
+
+// ======Section2 =======
+
+// ======Animacion de Rotacion de Imagenes======
+
+ScrollTrigger.create({
+    trigger: '.contenedor-imagen',
+    start: 'top center',
+    onEnter: () => {
+      gsap.to('.contenedor-imagen', {
+        rotation: -10,
+        duration: 1,
+        ease: 'power1.inOut'
+      });
+    }
+  });
+
+  ScrollTrigger.create({
+    trigger: '.frase-autor',
+    start: 'top center',
+    onEnter: () => {
+      gsap.from('.frase-autor', {
+        autoAlpha: 0,
+        duration: 2,
+        ease: 'power2.inOut'
+      });
+    }
+  });
