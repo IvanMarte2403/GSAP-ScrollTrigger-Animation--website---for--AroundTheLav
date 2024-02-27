b35afcd9dcded61096762911a18c23e9b84a126f
IvanMarte <ivanbiasfx@gmail.com>
2024-02-27 10:32:41 -0600

    Maquetación Menu 2.0

--- animaciones/menu.js
+++ animaciones/menu.js
@@ -1,29 +1,15 @@
-// JavaScript
-window.addEventListener('scroll', function() {
-    let links = document.querySelectorAll('.menu-link');
-    links.forEach(link => {
-      let section = document.querySelector(link.hash);
-      if (
-        section.offsetTop <= window.pageYOffset &&
-        section.offsetTop + section.offsetHeight > window.pageYOffset
-      ) {
-        link.classList.add('active');
-      } else {
-        link.classList.remove('active');
-      }
-    });
-  });
-  
-  let toggleMenuButton = document.getElementById('toggle-menu');
-  let showMenuButton = document.getElementById('show-menu');
-  let menu = document.getElementById('menu');
-  
-  toggleMenuButton.addEventListener('click', function() {
-    menu.style.display = 'none';
-    showMenuButton.style.display = 'block';
-  });
-  
-  showMenuButton.addEventListener('click', function() {
-    menu.style.display = 'block';
-    showMenuButton.style.display = 'none';
-  });
+var menu = document.getElementById('menuLateral');
+var boton = document.getElementById('botonMenu');
+var closebtn = document.querySelector('.closebtn');
+
+boton.addEventListener('click', function() {
+    if (menu.style.width === '250px') {
+        menu.style.width = '0';
+    } else {
+        menu.style.width = '250px';
+    }
+});
+
+closebtn.addEventListener('click', function() {
+    menu.style.width = '0';
+});
