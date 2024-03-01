58ba5345c4cdfdab456f4389d43b791d7e96ef55
IvanMarte <ivanbiasfx@gmail.com>
2024-02-29 22:23:11 -0600

    Primer Versión Subida

--- animaciones/seccion1.js
+++ animaciones/seccion1.js
@@ -65,3 +65,152 @@ logos.forEach((logo, i) => {
     }
   });
 });
+
+
+
+// ======Frase Te entendemos como nadie ======
+
+gsap.set('.row-three h1', { scale: 0.8 });
+gsap.set('.row-three span', { y: '-100vh' });
+
+ScrollTrigger.create({
+  trigger: '.row-three h1',
+  start: 'top center',
+  onEnter: () => {
+    gsap.to('.row-three h1', { scale: 1, duration: 1 });
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '.row-three span',
+  start: 'top center',
+  onEnter: () => {
+    gsap.to('.row-three span', { y: '0', duration: 1 });
+  }
+});
+
+let h1s = document.querySelectorAll('.three-div-h1 h1');
+
+h1s.forEach((h1, i) => {
+  ScrollTrigger.create({
+    trigger: h1,
+    start: 'top 90%',
+    onEnter: () => {
+      let text = h1.textContent;
+      h1.textContent = '';
+      h1.style.visibility = 'visible';
+      let i = 0;
+      let typing = setInterval(() => {
+        if (i < text.length) {
+          h1.textContent += text[i];
+          i++;
+        } else {
+          clearInterval(typing);
+        }
+      }, 100); // Ajusta la velocidad de la animación aquí
+    }
+  });
+});
+
+
+gsap.set('.four-titulo h1', { autoAlpha: 0 });
+
+ScrollTrigger.create({
+  trigger: '.four-titulo h1',
+  start: 'top center',
+  onEnter: () => {
+    gsap.to('.four-titulo h1', { autoAlpha: 1, duration: 1 });
+  }
+});
+
+gsap.set('.four-contenedor-imagenes img', { autoAlpha: 0 });
+
+let imgs = document.querySelectorAll('.four-contenedor-imagenes img');
+
+imgs.forEach((img, i) => {
+  ScrollTrigger.create({
+    trigger: img,
+    start: 'top center',
+    onEnter: () => {
+      gsap.to(img, { autoAlpha: 1, duration: 1 });
+    }
+  });
+});
+
+
+gsap.set('.texto-carousel p', { autoAlpha: 0, scale: 0.8 });
+
+ScrollTrigger.create({
+  trigger: '.texto-carousel p',
+  start: 'top center',
+  onEnter: () => {
+    gsap.to('.texto-carousel p', { autoAlpha: 1, scale: 1, duration: 1 });
+  }
+});
+
+gsap.set('.four-frase1 h1', { x: '100vw' });
+
+ScrollTrigger.create({
+  trigger: '.four-frase1 h1',
+  start: 'top center',
+  onEnter: () => {
+    gsap.to('.four-frase1 h1', { x: '0', duration: 1 });
+  },
+  onLeaveBack: () => {
+    gsap.to('.four-frase1 h1', { x: '100vw', duration: 1 });
+  }
+});
+
+gsap.set('.four-leyendo h1', { x: '-100vw' });
+
+ScrollTrigger.create({
+  trigger: '.four-leyendo h1',
+  start: 'top 75%',
+  onEnter: () => {
+    gsap.to('.four-leyendo h1', { x: '0', duration: 1 });
+  },
+  onLeaveBack: () => {
+    gsap.to('.four-leyendo h1', { x: '-100vw', duration: 1 });
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '.four-leyendo2 h1',
+  start: 'top 75%',
+  onEnter: () => {
+    gsap.to('.four-leyendo2 h1', { y: '0', duration: 1, delay: 0.5 });
+  },
+  onLeaveBack: () => {
+    gsap.to('.four-leyendo2 h1', { y: '100vh', duration: 1 });
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '.four-leyendo2 h1',
+  start: 'top 80%',
+  onEnter: () => {
+    let h1 = document.querySelector('.four-leyendo2 h1');
+    let text = h1.textContent;
+    h1.textContent = '';
+    let i = 0;
+    let typing = setInterval(() => {
+      if (i < text.length) {
+        h1.textContent += text[i];
+        i++;
+      } else {
+        clearInterval(typing);
+      }
+    }, 60); // Ajusta la velocidad de la animación aquí
+  }
+});
+
+
+gsap.set('.four-frase3 h1', { autoAlpha: 0 });
+
+ScrollTrigger.create({
+  trigger: '.four-frase3 h1',
+  start: 'top center',
+  onEnter: () => {
+    gsap.to('.four-frase3 h1', { autoAlpha: 1, duration: 1 });
+  }
+});
